af98e12cadbcffccadf9f67556825a6a3a611aa4
alemania123 <58455076+alemania123@users.noreply.github.com>
2022-05-17 19:10:48 +0800

    added database

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from asyncio import create_task
+from unicodedata import name
 from urllib import response
-from flask import Flask, make_response,send_file,render_template,request
+from flask import Flask, make_response,send_file,render_template,request,jsonify
+
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 from flask_mail import Mail, Message
@@ -7,13 +10,57 @@ from config import mail_username, mail_password
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import (create_engine, MetaData, Table,Column, Integer, String)
+from sqlalchemy import (create_engine, MetaData, Table, Column, Integer, String, DateTime)
+
+
 
 # klheuklzofurhpfs
-app = Flask(__name__, template_folder="./templates")
-mail = Mail(app)
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyDNc_aHa9fNKbTS1amE0gSO2ouOj9trU8U"
 
+app = Flask(__name__, template_folder="./templates")
+
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyDNc_aHa9fNKbTS1amE0gSO2ouOj9trU8U"
 GoogleMaps(app) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/user_info'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+meta_data = MetaData()
+
+requestdataset = Table(
+    'requestdataset', 
+    meta_data, 
+    Column('id', Integer(), primary_key = True),
+    Column('name', String(255), nullable = False),    
+    Column('email', String(255), nullable = False),
+    Column('organization', String(255), nullable = False),
+    Column('message', String(255), nullable = False),
+    Column('attachment', String(255), nullable = False),
+) 
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+meta_data.create_all(engine)
+
+try:
+    conn = engine.connect()
+    print('db connected')
+    print('connection object is :{}' .format(conn))
+except:
+    print('db not connected')
+    
+# class requestdataset(db.Model):
+#     id = db.Column(db.Integer, primary_key=True),
+#     name = db.Column(db.String(255), nullable = True),
+#     email = db.Column(db.String(255), nullable = True),
+#     organization = db.Column(db.String(255), nullable=True),
+#     message = db.Column(db.String(255), primary_key=True),
+#     attachment = db.Column(db.String(255), primary_key=True),
+
+
+
+mail = Mail(app)
 
 app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = "587"
@@ -92,9 +139,27 @@ def view():
         email = request.form.get('email')
         organization = request.form.get('organization')
         message = request.form.get('message')
-        msg = Message(subject  = f"Mail from {name}", body = f" Name: {name}\n E-Mail: {email}\n Organization:{organization}\n\nMessage: {message}",  sender = mail_username, recipients = ['alemaniacamilleite111@gmail.com'])
-        mail.send(msg)
-        return render_template('view.html', success=True)
+        # msg = Message(subject  = f"Mail from {name}", body = f" Name: {name}\n E-Mail: {email}\n Organization:{organization}\n\nMessage: {message}",  sender = mail_username, recipients = ['alemaniacamilleite111@gmail.com'])
+        # # mail.send(msg)
+
+        qry = requestdataset.insert().values(
+            name = request.form.get('name'), 
+            email = request.form.get('email'),
+            organization = "CCS", 
+            message = "hello world", 
+            attachment = "dataset.csv", 
+        )
+
+        # print(str(ins))
+        # print(ins.compile().params)
+        result = conn.execute(qry)
+    
+        print(result)
+        # print('Last inserted key:')
+        # print(result.inserted_primary_key)
+
+        resp = {'message': 'Successfull request', 'name': name}
+        return make_response(jsonify(resp), 200)
     return render_template("view.html")
 
     #     # name = request.form.get('name')
