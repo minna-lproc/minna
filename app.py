23a609070cd356b0fa7953c304df52d3d68c2b44
alemania123 <58455076+alemania123@users.noreply.github.com>
2022-05-19 10:53:30 +0800

    working database

--- app.py
+++ app.py
@@ -2,93 +2,41 @@ from asyncio import create_task
 from unicodedata import name
 from urllib import response
 from flask import Flask, make_response,send_file,render_template,request,jsonify
-
+import pymysql
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 from flask_mail import Mail, Message
-from config import mail_username, mail_password
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
-from flask_sqlalchemy import SQLAlchemy
-# from flask_sqlalchemy import (create_engine, MetaData, Table,Column, Integer, String)
-from sqlalchemy import (create_engine, MetaData, Table, Column, Integer, String, DateTime)
-
-
+from werkzeug.utils import secure_filename
 
 # klheuklzofurhpfs
 
 app = Flask(__name__, template_folder="./templates")
 
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyDNc_aHa9fNKbTS1amE0gSO2ouOj9trU8U"
-GoogleMaps(app) 
+# Configurations
+app.config.from_object('config')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/user_info'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-meta_data = MetaData()
-
-requestdataset = Table(
-    'requestdataset', 
-    meta_data, 
-    Column('id', Integer(), primary_key = True),
-    Column('name', String(255), nullable = False),    
-    Column('email', String(255), nullable = False),
-    Column('organization', String(255), nullable = False),
-    Column('message', String(255), nullable = False),
-    Column('attachment', String(255), nullable = False),
-) 
-
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-meta_data.create_all(engine)
-
-try:
-    conn = engine.connect()
-    print('db connected')
-    print('connection object is :{}' .format(conn))
-except:
-    print('db not connected')
-    
-# class requestdataset(db.Model):
-#     id = db.Column(db.Integer, primary_key=True),
-#     name = db.Column(db.String(255), nullable = True),
-#     email = db.Column(db.String(255), nullable = True),
-#     organization = db.Column(db.String(255), nullable=True),
-#     message = db.Column(db.String(255), primary_key=True),
-#     attachment = db.Column(db.String(255), primary_key=True),
-
-
-
+GoogleMaps(app)
 mail = Mail(app)
 
-app.config['MAIL_SERVER'] = "smtp.gmail.com"
-app.config['MAIL_PORT'] = "587"
-app.config['MAIL_USE_TLS'] = "True"
-app.config['MAIL_USE_SSL'] = "False"
-app.config['MAIL_USERNAME'] = mail_username
-app.config['MAIL_PASSWORD'] = mail_password
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-# HOST_NAME = "localhost"
-# username = "joe.clio001@gmail.com"
-# password = "!purps#9932CC"
-# mail_from = "joe.clio001@gmail.com"
-# mail_to = "alemaniacamilleite111@gmail.com"
-# mail_subject = "Requested datasets"
-# mail_body = "This is a test message"
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# mimemsg = MIMEMultipart()
-# mimemsg['From']=mail_from
-# mimemsg['To']=mail_to
-# mimemsg['Subject']=mail_subject
-# mimemsg.attach(MIMEText(mail_body, 'plain'))
-# connection = smtplib.SMTP(host='smtp.gmail.com', port=587)
-# connection.starttls()
-# connection.login(username,password)
-# connection.send_message(mimemsg)
-# connection.quit()
+try:
+    connection = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='minna_local',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+except:
+    print('db not connected')
 
-@app.route("/") 
+@app.route("/")
 
 @app.route("/index")
 def index():
@@ -107,19 +55,19 @@ def people():
     return render_template('people.html')
 
 @app.route("/process_form")
-def process_form(): 
+def process_form():
     return render_template("process_form.html")
 
 # @app.route("/send", methods=["POST"])
-# def send(): 
+# def send():
 #     mail = MIMEMultipart("alternative")
 #     mail["Subject"] = mail_subject
 #     mail["From"] = mail_from
 #     mail["To"] = mail_to
-    
+
 #     data = dict(request.form)
 #     msg = "<html><head></head><body>"
-#     for key, value in data.items(): 
+#     for key, value in data.items():
 #         msg += key + " : " + value + "<br>"
 #         msg += "</body></html>"
 #         mail.attach(MIMEText(msg, "html"))
@@ -127,34 +75,39 @@ def process_form():
 #         mailer = smtplib.SMTP("localhost")
 #         mailer.sendmail(mail_from. mail_to, mail.as_string())
 #         mailer.quit()
-        
+
 #         res = make_response("OK", 200)
 #         return res
 
 
 @app.route("/view", methods = ['GET', 'POST'])
 def view():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         organization = request.form.get('organization')
         message = request.form.get('message')
+
+
         # msg = Message(subject  = f"Mail from {name}", body = f" Name: {name}\n E-Mail: {email}\n Organization:{organization}\n\nMessage: {message}",  sender = mail_username, recipients = ['alemaniacamilleite111@gmail.com'])
         # # mail.send(msg)
 
-        qry = requestdataset.insert().values(
-            name = request.form.get('name'), 
-            email = request.form.get('email'),
-            organization = "CCS", 
-            message = "hello world", 
-            attachment = "dataset.csv", 
-        )
+        f = request.files['file']
+        filename = f.filename
+        f.save(secure_filename(filename))
 
-        # print(str(ins))
-        # print(ins.compile().params)
-        result = conn.execute(qry)
-    
-        print(result)
+        with connection.cursor() as cursor:
+                prepared_qry = 'INSERT INTO `requestdataset` (name, email, organization, message, file) VALUES (%s, %s, %s, %s, %s)'
+                cursor.execute(prepared_qry,(name, email, organization, message, filename))
+                connection.commit()
+            # with connection.cursor() as cursor:
+            #     sql = "SELECT * FROM students"
+            #     cursor.execute(sql)
+            # for result in cursor.fetchall():
+            #     print(result, '<br>')
+        # finally:
+        #     connection.close()
+
         # print('Last inserted key:')
         # print(result.inserted_primary_key)
 
@@ -166,7 +119,7 @@ def view():
     #     # email = request.form.get('email')
     #     # organization = request.form.get('organization')
     #     # message = request.form.get('message')
-    #     # msg = Message(subject  = f"Mail from {name}", body = f" Name: {name} E-Mail: {email}\n Organization:{organization}\n\nMessage: {message}", 
+    #     # msg = Message(subject  = f"Mail from {name}", body = f" Name: {name} E-Mail: {email}\n Organization:{organization}\n\nMessage: {message}",
     #     # sender = mail_username, recipients = ['"alemaniacamilleite111@gmail.com"'])
     #     # mail.send(msg)
     # # try:
@@ -180,9 +133,9 @@ def view():
     # #     result ['name'] = request.form['name']
     # #     result ['email'] = request.form['email'].replace(' ', '').lower()
     # #     result ['message'] = request.form['message']
-    
+
     # # sendTestEmail(result)
-  
+
     # return render_template('view.html', success=True)
     # # return render_template('view.html', ** locals())
 
@@ -227,10 +180,9 @@ def news03():
 #     return render_template('./process_form')
 
 @app.route("/download_files")
-def download_files(): 
- p = "../sample.txt"
- return send_file(p, as_attachment=True)
+def download_files():
+    p = "../sample.txt"
+    return send_file(p, as_attachment=True)
+
 if __name__ == "__main__":
         app.run(debug=True)
-
-    
--- config.py
+++ config.py
@@ -1,2 +1,35 @@
-mail_username = "test.smptp404@gmail.com"
-mail_password = "testingpassword123"
+import os
+
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+UPLOAD_PATH = os.path.join(BASE_DIR, 'storage')
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+# UPLOAD_EXTENSIONS
+GOOGLEMAPS_KEY = "AIzaSyDNc_aHa9fNKbTS1amE0gSO2ouOj9trU8U"
+
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = "587"
+MAIL_USE_TLS = "True"
+MAIL_USE_SSL = "False"
+MAIL_USERNAME = "test.smptp404@gmail.com"
+MAIL_PASSWORD = "testingpassword123"
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+THREADS_PER_PAGE = 2
+
+# Enable protection agains *Cross-site Request Forgery (CSRF)*
+CSRF_ENABLED     = True
+
+# Use a secure, unique and absolutely secret key for
+# signing the data.
+CSRF_SESSION_KEY = "secret"
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
